b6b01293975a7b2f8efe5f42658fa2cac152ccd6
Joel D Souza <joeljeevan10@gmail.com>
2026-02-26 02:30:56 +0530

    added type conv, secuirity filters

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -2,11 +2,23 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { HttpExceptionFilter } from './common/filters/http-exception.filter';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.setGlobalPrefix('/api');
+
+  // added validations, type handling
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+      transformOptions: { enableImplicitConversion: true },
+    }),
+  );
+
   app.useGlobalFilters(new HttpExceptionFilter());
   app.enableCors({ origin: '*' });
 
